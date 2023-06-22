27365af59fc9349e3c6c3f7daf0bd6fa3b2e093b
= <=>
2023-06-22 13:58:32 +0700

    feat: add competition page

---
+++ src/components/MainCompetition.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function MainCompetition() {
+  return (
+    <section>
+      <div className="bg-competition h-screen  w-full bg-cover bg-fixed bg-no-repeat text-slate-50">
+        <div className="">
+          <div className="absolute bottom-72 w-full p-10 md:bottom-40">
+            <h1 className="text-center text-[30px] tracking-widest md:p-5 md:text-[80px]">
+              KKTCBN 2023
+            </h1>
+            <p className="text-center text-[15px] font-medium md:p-2 md:text-[30px]">
+              Kontes Kapal Cepat Tak Berawak
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/competition.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+import Layout from "@/components/layout/Layout";
+import Seo from "@/components/Seo";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import MainCompetition from "@/components/MainCompetition";
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <Navbar />
+        <MainCompetition />
+        <Footer />
+      </main>
+    </Layout>
+  );
+}
